47258b3c2dec86169371bc9e35973011be8f5b5e
1998-droid <tiotiotio98@gmail.com>
2022-07-14 11:02:48 +0700

    commit

--- auth/log2.php
+++ auth/log2.php
@@ -18,7 +18,7 @@
     
 
     <div class="wrapper fadeInDown">
-      <marquee behavior="" direction="">NIK TIDAK PAKE TITIK</marquee>
+      <marquee behavior="" direction="left">NIK TIDAK PAKE TITIK</marquee>
   <div id="formContent">
     <!-- Tabs Titles -->
     <h2><b><a href="daf2.php" class="active underline"> Daftar </a></b></h2>
